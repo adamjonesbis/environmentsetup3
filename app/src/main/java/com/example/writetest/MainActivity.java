4255e9c495c56b5f0f2bd1a7ed04babeb7fa2f90
adamj <adamjones03@gmail.com>
2022-10-26 19:17:57 +0100

    first commit

---
+++ app/src/main/java/com/example/writetest/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.writetest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText input;
+    private Button btn;
+    private DatabaseReference rootDatabaseref;
+    private TextView textView;
+    private Button btnRead;
+
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input=findViewById(R.id.input);
+        btn=findViewById(R.id.btn);
+        btnRead=findViewById(R.id.btnRead);
+        textView=findViewById(R.id.textView);
+        
+        rootDatabaseref= FirebaseDatabase.getInstance().getReference().child("MyData");
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                rootDatabaseref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists())
+                        {
+                            String data=dataSnapshot.getValue().toString();
+                            textView.setText(data);
+                        }
+                        }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+            });
+        }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data=input.getText().toString();
+                rootDatabaseref.setValue(data);
+
+            }
+
+
+        });
+    }
+}
